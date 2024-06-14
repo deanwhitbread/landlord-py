252a2c4bfd29b6202a09287b4c2442b7b8a42799
Dean Whitbread <dean_whitbread@hotmail.com>
2024-06-14 11:41:57 +0100

    Add converter to convert intergers in hands to Card objects.
    - Implement helper function to convert number to Card object.
    - Implement helper function to convert the collection of hands to contain a list of Card objects.
    - Add helper function to all card hands tests.

--- tests/test_card_deck_and_hands.py
+++ tests/test_card_deck_and_hands.py
@@ -1,11 +1,13 @@
 import unittest
-from card_deck import CardDeck
+from card_deck import CardDeck, Card
 from card_hand import CardHand 
 
 class CardDeckTestCase(unittest.TestCase):
     def setUp(self):
         self.card_numbers = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}
         self.suits = {"hearts", "diamonds", "clubs", "spades", "joker"}
+        self.number_points_dict = {3:1, 4:2, 5:3, 6:5, 7:8, 8:13, 9:21, 
+            10:34, 11:55, 12:89, 13:144, 1:233, 2:377, 14:610, 15:987}
 
     def tearDown(self):
         del self.card_numbers
@@ -76,12 +78,10 @@ class CardDeckTestCase(unittest.TestCase):
         '''Test point ranking system uses fibonacci sequence.'''
         self.assertEqual(len(self.__class__.deck._card_stack), 54)
 
-        number_points_dict = {3:1, 4:2, 5:3, 6:5, 7:8, 8:13, 9:21, 
-            10:34, 11:55, 12:89, 13:144, 1:233, 2:377, 14:610, 15:987}
         for card in self.__class__.deck._card_stack:
             points = card.get_points()
             number = card.get_number()
-            self.assertEqual(points, number_points_dict[number])
+            self.assertEqual(points, self.number_points_dict[number])
 
     def test_point_ranking_on_card_number_value(self):
         '''Test that the order of points for card numbers following the 
@@ -89,12 +89,11 @@ class CardDeckTestCase(unittest.TestCase):
         '''
         self.assertEqual(len(self.__class__.deck._card_stack), 54)
 
-        number_points_dict = {3:1, 4:2, 5:3, 6:5, 7:8, 8:13, 9:21, 
-            10:34, 11:55, 12:89, 13:144, 1:233, 2:377, 14:610, 15:987}
+        
         for card in self.__class__.deck._card_stack:
             key = card.get_number()
             points = card.get_points()
-            self.assertEqual(points, number_points_dict[key])
+            self.assertEqual(points, self.number_points_dict[key])
 
     '''
         Card Hands
@@ -104,6 +103,8 @@ class CardDeckTestCase(unittest.TestCase):
         hands = [[1],[2],[3],[4],[5],[6],[7],[8],[9],
             [10],[11],[12],[13],[14],[15]]
         
+        hands = self._convert_hand_numbers_to_card_objects(hands)
+
         for hand in hands:
             self.__class__.hand.select(hand)
             self.assertTrue(self.__class__.hand.play())
@@ -113,6 +114,8 @@ class CardDeckTestCase(unittest.TestCase):
         hands = [[3,4,5,6,7],[4,5,6,7,8],[5,6,7,8,9], 
             [6,7,8,9,10],[7,8,9,10,11],[8,9,10,11,12],
             [9,10,11,12,13],[10,11,12,13,1]]
+
+        hands = self._convert_hand_numbers_to_card_objects(hands)
         
         for hand in hands:
             self.__class__.hand.select(hand)
@@ -122,6 +125,8 @@ class CardDeckTestCase(unittest.TestCase):
         '''Test that solo chain is at least 5 consecutive cards.'''
         hands = [[7,8,9,10,11],[3,4,5,6,7,8],[5,6,7,8,9,10], 
             [7,8,9,10,11,12,13,1],[3,4,5,6,7,8,9,10,11,12,13,1]]
+
+        hands = self._convert_hand_numbers_to_card_objects(hands)
         
         for hand in hands:
             self.__class__.hand.select(hand)
@@ -132,6 +137,8 @@ class CardDeckTestCase(unittest.TestCase):
         hands = [[3,4,5,6],[5,6,7,8],[9,10,11,12],[11,12,13,1,2], 
             [12,13,1,2,14],[13,1,2,14,15],[1,2,3,4,5],[2,3,4,5,6], 
             [3,4,6,7,8],[7,8,10,12,1]]
+
+        hands = self._convert_hand_numbers_to_card_objects(hands)
         
         for hand in hands:
             self.__class__.hand.select(hand)
@@ -142,6 +149,8 @@ class CardDeckTestCase(unittest.TestCase):
         hands = [[3,3],[4,4],[5,5],[6,6],[7,7],[8,8],[9,9],[10,10],
             [11,11],[12,12],[13,13],[1,1],[2,2]]
 
+        hands = self._convert_hand_numbers_to_card_objects(hands)
+
         for hand in hands:
             self.__class__.hand.select(hand)
             self.assertTrue(self.__class__.hand.play())
@@ -149,6 +158,8 @@ class CardDeckTestCase(unittest.TestCase):
     def test_invalid_pair_card_hand(self):
         '''Test all invalid pair card hands.'''
         hands = [[4,1],[1,4],[10,5],[3,4],[8,7]]
+
+        hands = self._convert_hand_numbers_to_card_objects(hands)
 
         for hand in hands:
             self.__class__.hand.select(hand)
@@ -160,6 +171,8 @@ class CardDeckTestCase(unittest.TestCase):
             [7,7,8,8,9,9],[8,8,9,9,10,10],[9,9,10,10,11],[10,10,11,11,12,12],
             [11,11,12,13,13],[12,12,13,13,1,1]]
 
+        hands = self._convert_hand_numbers_to_card_objects(hands)
+
         for hand in hands:
             self.__class__.hand.select(hand)
             self.assertTrue(self.__class__.hand.play())
@@ -168,6 +181,8 @@ class CardDeckTestCase(unittest.TestCase):
         '''Test that the pair chain is at least three consecutive pairs.'''
         hands = [[6,6,7,7,8,8],[4,4,5,5,6,6,7,7,8,8,9,9],[10,10,11,11,12,12,13,13],
             [5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12,13,13,1,1]]
+
+        hands = self._convert_hand_numbers_to_card_objects(hands)
         
         for hand in hands:
             self.__class__.hand.select(hand)
@@ -178,6 +193,8 @@ class CardDeckTestCase(unittest.TestCase):
         hands = [[5,5,7,7],[8,8,9,9,10,10],[1,1,2,2,14,14],[2,2,14,14,15,15],
             [6,6,7,7,8,8,9,9,10,10,11,11,12,12,13,1,1,2,2],[2,2,3,3]]
 
+        hands = self._convert_hand_numbers_to_card_objects(hands)
+
         for hand in hands:
             self.__class__.hand.select(hand)
             self.assertFalse(self.__class__.hand.play())
@@ -186,6 +203,8 @@ class CardDeckTestCase(unittest.TestCase):
         '''Test all valid trio card hands.'''
         hands = [[3,3,3],[4,4,4],[5,5,5],[6,6,6],[7,7,7],[8,8,8],[9,9,9],
             [10,10,10],[11,11,11],[12,12,12],[13,13,13],[1,1,1],[2,2,2]]
+
+        hands = self._convert_hand_numbers_to_card_objects(hands)
         
         for hand in hands:
             self.__class__.hand.select(hand)
@@ -194,6 +213,8 @@ class CardDeckTestCase(unittest.TestCase):
     def test_invalid_trio_card_hand(self):
         '''Test all invalid trio card hands.'''
         hands= [[3,4,5],[6,6,9],[10,10,1]]
+
+        hands = self._convert_hand_numbers_to_card_objects(hands)
 
         for hand in hands:
             self.__class__.hand.select(hand)
@@ -204,6 +225,8 @@ class CardDeckTestCase(unittest.TestCase):
         hands = [[3,3,3,6],[7,7,7,3],[6,6,6,1],[1,1,1,4],[2,2,2,3],
             [2,2,2,14], [2,2,2,15],[11,11,11,1]]
 
+        hands = self._convert_hand_numbers_to_card_objects(hands)
+
         for hand in hands:
             self.__class__.hand.select(hand)
             self.assertTrue(self.__class__.hand.play())
@@ -211,6 +234,8 @@ class CardDeckTestCase(unittest.TestCase):
     def test_trio_and_pair_card_hand(self):
         '''Test all valid trio with pair card hands.'''
         hands = [[3,3,3,4,4],[7,7,7,10,10],[1,1,1,3,3],[2,2,2,7,7]]
+
+        hands = self._convert_hand_numbers_to_card_objects(hands)
 
         for hand in hands:
             self.__class__.hand.select(hand)
@@ -221,10 +246,18 @@ class CardDeckTestCase(unittest.TestCase):
         hands = [[3,3,3,4,4,4,5,5,6,6],[7,7,7,8,8,8,9,9,9,1,1,3,3,4,4],
             [11,11,11,12,12,12,13,13,13,1,1,1,5,5,4,4,3,3,9,9], [1,1,1,6,6]]
 
+        hands = self._convert_hand_numbers_to_card_objects(hands)
+
+        for hand in hands:
+            self.__class__.hand.select(hand)
+            self.assertTrue(self.__class__.hand.play())
+
     def test_invalid_trio_and_pair_chain_card_hand(self):
         '''Test all invalid trio with pair chain card hands.'''
         hands = [[2,2,2,14,15],[3,3,3,4,4,4,8,8],[6,6,6,7,7,7,3,3,10,11],
             [1,1,1,2,2,2,5,5,8,8]]
+        
+        hands = self._convert_hand_numbers_to_card_objects(hands)
 
         for hand in hands:
             self.__class__.hand.select(hand)
@@ -235,6 +268,8 @@ class CardDeckTestCase(unittest.TestCase):
         hands = [[3,3,3,4,4,4],[4,4,4,5,5,5],[6,6,6,7,7,7],[8,8,8,9,9,9],
             [10,10,10,11,11,11],[11,11,11,12,12,12],[13,13,13,1,1,1]]
 
+        hands = self._convert_hand_numbers_to_card_objects(hands)
+
         for hand in hands:
             self.__class__.hand.select(hand)
             self.assertTrue(self.__class__.hand.play())
@@ -242,6 +277,9 @@ class CardDeckTestCase(unittest.TestCase):
     def test_invalid_airplane_card_hand(self):
         '''Test all invalid airplane chain hands.'''
         hands = [[2,2,2,3,3,3], [1,1,1,2,2,2]]
+
+        hands = self._convert_hand_numbers_to_card_objects(hands)
+
         for hand in hands:
             self.__class__.hand.select(hand)
             self.assertFalse(self.__class__.hand.play())
@@ -250,6 +288,8 @@ class CardDeckTestCase(unittest.TestCase):
         '''Test all valid trio with solo and airplane card hands.'''
         hands = [[3,3,3,4,4,4,7,11], [10,10,10,11,11,11,3,4], 
             [10,10,10,11,11,11,12,12,12,13,13,13,1,1,1,3,15,6,7,2]]
+
+        hands = self._convert_hand_numbers_to_card_objects(hands)
 
         for hand in hands:
             self.__class__.hand.select(hand)
@@ -260,6 +300,8 @@ class CardDeckTestCase(unittest.TestCase):
         hands = [[2,2,2,3,3,3,4,4,4,8,4,6],[1,1,1,2,2,2,5,6],
             [4,4,4,5,5,5,7,7,8,8]]
 
+        hands = self._convert_hand_numbers_to_card_objects(hands)
+
         for hand in hands:
             self.__class__.hand.select(hand)
             self.assertFalse(self.__class__.hand.play())
@@ -267,6 +309,8 @@ class CardDeckTestCase(unittest.TestCase):
     def test_bomb_card_hand(self):
         '''Test all valid bomb card hands.'''
         hands = [[3,3,3,3], [5,5,5,5], [2,2,2,2], [7,7,7,7]]
+
+        hands = self._convert_hand_numbers_to_card_objects(hands)
 
         for hand in hands:
             self.__class__.hand.select(hand)
@@ -276,6 +320,8 @@ class CardDeckTestCase(unittest.TestCase):
         '''Test all valid bomb with solo card hands.'''
         hands = [[12,12,12,12,5,4],[9,9,9,9,14,6]]
 
+        hands = self._convert_hand_numbers_to_card_objects(hands)
+
         for hand in hands:
             self.__class__.hand.select(hand)
             self.assertTrue(self.__class__.hand.play())
@@ -284,6 +330,8 @@ class CardDeckTestCase(unittest.TestCase):
         '''Test all invalid bomb with solo card hands.'''
         hands = [[10,10,10,10,2], [5,5,5,5,14,15]]
 
+        hands = self._convert_hand_numbers_to_card_objects(hands)
+
         for hand in hands:
             self.__class__.hand.select(hand)
             self.assertFalse(self.__class__.hand.play())
@@ -291,6 +339,8 @@ class CardDeckTestCase(unittest.TestCase):
     def test_valid_bomb_dual_pair_card_hand(self):
         '''Test all valid bomb with dual pair card hands.'''
         hands = [[5,5,5,5,13,13,1,1], [7,7,7,7,12,12,8,8]]
+
+        hands = self._convert_hand_numbers_to_card_objects(hands)
 
         for hand in hands:
             self.__class__.hand.select(hand)
@@ -301,14 +351,48 @@ class CardDeckTestCase(unittest.TestCase):
         hands = [[5,5,5,5,6,6,6,6], [12,12,12,12,3,3,3,3],
             [3,3,3,3,13,13,13,13]]
 
+        hands = self._convert_hand_numbers_to_card_objects(hands)
+
         for hand in hands:
             self.__class__.hand.select(hand)
             self.assertFalse(self.__class__.hand.play())
 
     def test_rocket_card_hand(self):
         '''Test rocked card hand.'''
-        self.__class__.hand.select([15,14])
+        hand = [15,14]
+        for i in range(len(hand)):
+            hand[i] = self._create_card_object_for_card_number(hand[i])
+
+        self.__class__.hand.select(hand)
         self.assertTrue(self.__class__.hand.play())
+
+    def _create_card_object_for_card_number(self, number, suit="joker"):
+        '''Create a Card object using the card number provided.
+        
+        Args: 
+            number - An integer representing the card number.
+            suit - A string representing the card suit. Default is joker.
+
+        Returns: A Card object. 
+        '''
+        return Card(number=number, 
+            suit=suit, 
+            points=self.number_points_dict[number])
+
+    def _convert_hand_numbers_to_card_objects(self, hands, suit="hearts"):
+        '''Helper function to convert the numbers in the hand to card objects.
+        
+        Args:
+            hands - A 2-dimensional list containing a list of integers. 
+            suit - The suit of all the cards. Default is hearts. 
+        
+        Returns: A 2-dimensional list containing a list of Card objects.
+        '''
+        for i in range(len(hands)):
+            for j in range(len(hands[i])):
+                hands[i][j] = self._create_card_object_for_card_number(hands[i][j], suit)
+        
+        return hands
 
     '''
         Card Hand Ranking
