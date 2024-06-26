c15f250cc06ca3cd8606b49ed1d3657dcae785aa
Dean Whitbread <dean_whitbread@hotmail.com>
2024-06-26 22:06:41 +0100

    Add template for Player class.
    - Add template with bid and get_stake_amount functions.
    - Add tests for the bid and get_stake_amount functions.

---
+++ game_player.py
@@ -0,0 +1,10 @@
+
+class Player:
+    def __init__(self):
+        pass
+
+    def bid(self, amount):
+        pass
+
+    def get_stake_amount(self):
+        pass
---
+++ tests/test_player_class.py
@@ -0,0 +1,49 @@
+import unittest
+from game_player import Player
+
+class CardDeckTestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+    
+    @classmethod
+    def setUpClass(cls):
+        cls.player = Player()
+
+    @classmethod
+    def tearDownClass(cls):
+        del cls.player 
+    
+    def test_can_submit_valid_bid(self):
+        self.assertTrue(self.player.bid(0)) 
+        self.assertTrue(self.player.bid(1)) 
+        self.assertTrue(self.player.bid(2))
+        self.assertTrue(self.player.bid(3))
+    
+    def test_cannot_submit_invalid_bid(self):
+        self.assertFalse(self.player.bid(4))
+        self.assertFalse(self.player.bid(10))
+        self.assertFalse(self.player.bid(20))
+    
+    def test_stake_decreases_when_valid_bid_submitted(self):
+        self.assertEqual(self.player.get_stake_amount(), 60)
+        self.player.bid(0)
+        self.assertEqual(self.player.get_stake_amount(), 60)
+        self.player.bid(1)
+        self.assertEqual(self.player.get_stake_amount(), 59)
+        self.player.bid(2)
+        self.assertEqual(self.player.get_stake_amount(), 57)
+        self.player.bid(3)
+        self.assertEqual(self.player.get_stake_amount(), 54)
+    
+    def test_stake_is_unchanged_when_invalid_bid_submitted(self):
+        self.assertEqual(self.player.get_stake_amount(), 60)
+        self.player.bid(4)
+        self.assertEqual(self.player.get_stake_amount(), 60)
+        self.player.bid(10)
+        self.assertEqual(self.player.get_stake_amount(), 60)
+        self.player.bid(20)
+        self.assertEqual(self.player.get_stake_amount(), 60)
+    
