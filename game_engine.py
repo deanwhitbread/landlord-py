e9103d46d5c624f84eaef0412edbf8122c2dd4dc
Dean Whitbread <dean_whitbread@hotmail.com>
2024-07-02 17:45:32 +0100

    Refactor BiddingEngine class.
    - Add get_players function that returns the players set in the game.
    - Add function description and typing to function headers.
    - Refactor tests for the BiddingEngine class to use the get_players and get_cards functions.

--- game_engine.py
+++ game_engine.py
@@ -13,6 +13,7 @@ class LandlordGame:
         self.players = players
         self.bidding_engine = BiddingEngine()
         self.deck = CardDeck()
+        self.reset()
 
     def play(self) -> bool:
         '''Play a round of Landlord. True is returned if a round can be
@@ -120,13 +121,16 @@ class LandlordGame:
 
 class BiddingEngine:
     def __init__(self):
+        '''Construct a BiddingEngine object. The bidding engine handles the bidding
+        between players in a round of landlord.'''
         self.reset()
     
-    def execute_bidding_round(self):
-        bidding_order = self._get_bidding_order()
-        max_bid = 0
-        winning_bidder = None
-        for player in bidding_order:
+    def execute_bidding_round(self) -> (int, Player):
+        '''Simulates a round of bidding in a game of landlord. An interger
+        representing the maximum bid and a Player object representing the winning
+        bidder is returned.'''
+        max_bid, winning_bidder = 0, None
+        for player in self._get_bidding_order():
             if player.get_bid_amount() is None:
                 player.set_bid(player.get_random_bid_amount())
             
@@ -139,28 +143,40 @@ class BiddingEngine:
 
         return max_bid, winning_bidder
 
-    def _get_bidding_order(self):
+    def _get_bidding_order(self) -> list[Player]:
+        '''Returns a list of Player objects where the players are ordered in their
+        bidding order. A player bids first if they have the three of hearts card. 
+        Otherwise, the list of players in the game is returned.'''
         first_bidder = None
-        for player in self.players:
-            for card in player.cards:
+        for player in self.get_players():
+            for card in player.get_cards():
                 if card.get_number()==3 and card.get_suit()=="hearts":
                     first_bidder = player
                     break 
         
         if first_bidder:
             order = [first_bidder]
-            for player in self.players:
-                if player==first_bidder:
-                    continue
-                
-                order.append(player)
+            for player in self.get_players():
+                if player!=first_bidder:
+                    order.append(player)
             
             return order
         else:
-            return self.players
+            return self.get_players()
     
-    def set_players(self, players):
+    def set_players(self, players: list[Player]):
+        '''Sets the players playing the game of landlord.
+        
+        Args:
+            players - A list of Player objects that represent the players
+                    playing the game of landlord.
+        '''
         self.players = players
 
+    def get_players(self):
+        '''Returns a list of Player objects that represent the players in the game.'''
+        return self.players
+
     def reset(self):
+        '''Resets the bidding engine, ready for a new round of landlord.'''
         self.players = None
--- tests/test_bidding_engine.py
+++ tests/test_bidding_engine.py
@@ -41,16 +41,16 @@ class BiddingEngineTestCase(unittest.TestCase):
         max_bid, winner = self.engine.execute_bidding_round()
         self.assertTrue(max_bid>=0 and max_bid<=3)
         self.assertTrue(type(winner), Player)
+        self.assertIn(winner, self.engine.get_players())
     
     def test_execute_bidding_round_function_returns_matches_the_max_bid_with_the_winner_bidder(self):
         max_bid, winner = self.engine.execute_bidding_round()
-        for player in self.players:
+        for player in self.engine.get_players():
             if player==winner:
                 self.assertEqual(player.get_bid_amount(), max_bid)
     
     def test_bidders_must_raise_bid_when_bidding_for_wildcards(self):
-        order = self.engine._get_bidding_order()
-        player1, player2, player3 = order[0], order[1], order[2]
+        player1, player2, player3 = self.engine._get_bidding_order()
         player1.set_bid(3)
         player2.set_bid(3)
         player3.set_bid(3)
@@ -95,28 +95,17 @@ class BiddingEngineTestCase(unittest.TestCase):
 
     def test_first_bidder_is_the_player_with_the_three_of_hearts(self):
         first_bidder = None 
-        for card in self.player1.cards:
-            if card.get_number()==3 and card.get_suit()=="hearts":
-                first_bidder = self.player1
-                break 
-
-        if not first_bidder:
-            for card in self.player2.cards:
+        for player in self.engine.get_players():
+            for card in player.get_cards():
                 if card.get_number()==3 and card.get_suit()=="hearts":
-                    first_bidder = self.player2
-                    break 
-
-        if not first_bidder:
-            for card in self.player3.cards:
-                if card.get_number()==3 and card.get_suit()=="hearts":
-                    first_bidder = self.player3
+                    first_bidder = player
                     break 
 
         if not first_bidder:
             for i in range(len(self.wildcards)):
                 card = self.wildcards[i]
                 if card.get_number()==3 and card.get_suit()=="hearts":
-                    self.player1.cards[0], self.wildcards[i] = self.wildcards[i], self.player1.cards[0]
+                    self.player1.get_cards()[0], self.wildcards[i] = self.wildcards[i], self.player1.get_cards()[0]
                     first_bidder = self.player1
         
         order = self.engine._get_bidding_order()
