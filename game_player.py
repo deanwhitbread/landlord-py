31977a141ca74a1886f4b410578ceba7f854ed18
Dean Whitbread <dean_whitbread@hotmail.com>
2024-07-02 16:16:50 +0100

    Refactor Player class.
    - Add function descriptions to all class functions.
    - Add typing to function headers.

--- game_player.py
+++ game_player.py
@@ -1,13 +1,22 @@
-from card_hand import CardHand
+from card_hand import CardHand, Card
 import game_rules as rules
 import random 
 
 class Player:
     def __init__(self):
+        '''Construct a Player class with the initial stake initialised.'''
         self.set_stake_amount(60)
         self.reset()
 
-    def set_bid(self, amount):
+    def set_bid(self, amount: int) -> bool:
+        '''Set the players' bid to the amount provided. If the bid is valid 
+        but exceeds the players' total stake, the total state is returned.
+        
+        Args:
+            amount - An integer reppresenting the amount the player is bidding. 
+
+        Returns: True if the bid was set successfully, False otherwise. 
+        '''
         if amount>3 or (amount>self.get_stake_amount() and not self.get_stake_amount()):
             return False
 
@@ -20,7 +29,9 @@ class Player:
 
         return True
 
-    def get_random_bid_amount(self):
+    def get_random_bid_amount(self) -> int:
+        '''Return a random bid amount. If the bid is valid but exceeds the 
+        players' total stake, the total state is returned.'''
         probabilty = random.random()
         if probabilty>0.85:
             self.set_bid(3)
@@ -33,25 +44,45 @@ class Player:
         
         return self.get_bid_amount()
 
-    def get_bid_amount(self):
+    def get_bid_amount(self) -> int:
+        '''Return the players' bid for the round.'''
         return self.bid
 
-    def get_stake_amount(self):
+    def get_stake_amount(self) -> int:
+        '''Return the players' total stake.'''
         return self.stake
     
-    def set_stake_amount(self, amount):
+    def set_stake_amount(self, amount: int):
+        '''Set the players' stake to the provided amount.
+        
+        Args:
+            amount - An integer representing the players' total stake amount. 
+        '''
         self.stake = amount
 
-    def set_cards(self, cards):
+    def set_cards(self, cards: list[Card]):
+        '''Set the players' cards.
+        
+        Args:
+            cards - A list of Card objects that represent the players' cards.    
+        '''
         self.cards = cards
 
-    def add_wildcards(self, wildcards):
+    def add_wildcards(self, wildcards: list[Card]):
+        '''Add the wildcards to the players' cards.
+        
+        Args:
+            cards - A list of Card objects that represent the players' cards.    
+        '''
         self.cards.extend(wildcards)
 
-    def get_cards(self):
+    def get_cards(self) -> list[Card]:
+        '''Return the players' cards.'''
         return self.cards
 
     def reset(self):
+        '''Resets the players' hand played, cards being held by the player,
+        and their bid for the round.'''
         self.hand = CardHand()
         self.cards = None
         self.bid = None
